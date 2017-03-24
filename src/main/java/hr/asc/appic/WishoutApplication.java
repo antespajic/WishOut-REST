20ba5ea053fc238b101d11d5c2acb3f3a55e14b4
antespajic <ante.spajic@yahoo.com>
2017-03-24 22:17:23 +0100

    Initial commit

---
+++ src/main/java/hr/asc/appic/WishoutApplication.java
@@ -0,0 +1,12 @@
+package hr.asc.appic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WishoutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WishoutApplication.class, args);
+	}
+}
