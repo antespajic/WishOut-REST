0347ea75d80f8c6a91708c46837236ccb43eea15
hrvojebusic <hrvoje.busic@protonmail.com>
2017-03-28 12:15:21 +0200

    Mongo config added

---
+++ src/main/java/hr/asc/appic/Config/MongoConfiguration.java
@@ -0,0 +1,32 @@
+package hr.asc.appic.Config;
+
+import com.mongodb.Mongo;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+@Configuration
+@EnableMongoRepositories
+public class MongoConfiguration extends AbstractMongoConfiguration {
+
+    @Override
+    protected String getDatabaseName() {
+        return "appic-wishout";
+    }
+
+    @Override
+    public Mongo mongo() throws Exception {
+        MongoClientURI uri = new MongoClientURI("mongodb://appic:appicjezakon123@ds147377.mlab.com:47377/appic-wishout");
+        return new MongoClient(uri);
+    }
+
+    /*
+    @Override
+    protected String getMappingBasePackage() {
+        return "com.infobip";
+    }
+    */
+}
+
