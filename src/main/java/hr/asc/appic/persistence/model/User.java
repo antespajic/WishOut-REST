99d30ba19f1bf4089efe82d2c21a3784120e2064
antespajic <ante.spajic@yahoo.com>
2017-05-30 20:05:43 +0200

    Made email field unique

--- src/main/java/hr/asc/appic/persistence/model/User.java
+++ src/main/java/hr/asc/appic/persistence/model/User.java
@@ -5,6 +5,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -30,6 +31,7 @@ public class User {
     
 	@Id
     private String id;
+	@Indexed(unique=true)
     private String email;
     private String password;
     private String firstName;
