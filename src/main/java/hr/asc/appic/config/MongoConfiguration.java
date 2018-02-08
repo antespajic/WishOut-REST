af9efda4b438c0057060e404de8d5004956b9a13
Ante Spajic <ante.spajic@protonmail.com>
2018-02-08 20:17:59 +0100

    Update MongoConfiguration.java

--- src/main/java/hr/asc/appic/config/MongoConfiguration.java
+++ src/main/java/hr/asc/appic/config/MongoConfiguration.java
@@ -18,8 +18,6 @@ public class MongoConfiguration extends AbstractMongoConfiguration {
 
     @Override
     public Mongo mongo() throws Exception {
-        //MongoClientURI uri = new MongoClientURI("mongodb://appic:appicjezakon123@ds147377.mlab.com:47377/appic-wishout");
-        //return new MongoClient(uri);
         return new MongoClient("127.0.0.1", 27017);
     }
 }
